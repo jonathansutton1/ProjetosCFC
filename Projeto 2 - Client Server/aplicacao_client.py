c74ea89ec85da8c62be635fe2259fa68e9cb1a71
Jonathan Sutton <jonathans@al.insper.edu.br>
2022-03-04 12:34:23 -0300

    limpeza no código

--- Projeto 2 - Client Server/aplicacao_client.py
+++ Projeto 2 - Client Server/aplicacao_client.py
@@ -2,7 +2,7 @@
 # Camada Física da Computação
 #Carareto
 #11/08/2020
-#Aplicação
+#AplicaçãoClient
 ####################################################
 
 
@@ -12,19 +12,12 @@ import time
 import numpy as np
 import random
 
-serialName = "COM7"                  # Windows(variacao de)
+serialName = "COM5"                  # Windows(variacao de)
 soma =0
 def comando():
-    com1 = enlace('COM7')
+    com1 = enlace(serialName)
     dic_comandos = {b'\x00\xff\x00\xff': 4, b'\x00\xff\xff\x00':4, b'\xff':1, b'\x00':1, b'\xff\x00': 2, b'\x00\xff':2 }
-    comando1 = b'\x00\xff\x00\xff'
-    comando2 = b'\x00\xff\xff\x00'
-    comando3 = b'\xff'
-    comando4 = b'\x00'
-    comando5 = b'\xff\x00'
-    comando6 = b'\x00\xff'
-
-    lista_comandos = [comando1,comando2,comando3,comando4,comando5,comando6]
+ 
     lista_selecionada = []
     lista_bytes=[]
     i = 0
@@ -34,7 +27,6 @@ def comando():
         com1.disable()
     while i < numero_comandos:
         random_add = random.choice(list(dic_comandos.keys()))
-        #print(random_add)
         lista_selecionada.append(random_add)
         lista_bytes += ((dic_comandos[random_add]).to_bytes(1, byteorder='big'))
         i += 1
@@ -45,8 +37,6 @@ def comando():
 
     print(f'tamanho da lista de comandos: {len(lista_selecionada)}')
     print(f'tamanho da lista de bytes: {len(lista_bytes)}')
- 
-    
     
     t=0
     global soma 
@@ -59,7 +49,7 @@ def comando():
 
 def main():
     try:
-        com1 = enlace('COM7')
+        com1 = enlace('COM5')
         
         com1.enable()
 
@@ -68,22 +58,20 @@ def main():
         time.sleep(1)
   
         print("----------------------------------------------------------")
-        print("Comunicação aberta com sucesso! Vamos ao resto do projeto!")
+        print("Transmissão aberta com sucesso! Vamos ao resto do projeto!")
         print("----------------------------------------------------------")
 
         comandos, tamanhos = comando()
         print("Serao enviadas 2 listas: uma com os bytes, o outro com o comando")
-
-        print("Agora será carregada a imagem em um formato de bytes.")
         
-        #faça aqui uma conferência do tamanho do seu txBuffer, ou seja, quantos bytes serão enviados.
-        print(f"Comandos enviados: {len(comandos)}")       
+        print(f"Quantidade de comandos enviados: {len(comandos)}")       
             
-        #finalmente vamos transmitir os tados. Para isso usamos a funçao sendData que é um método da camada enlace.
-        #faça um print para avisar que a transmissão vai começar.
+ 
         if len(comandos) >= 1:
             print("Lista carregada com sucesso! Vamos a transmissão.")
+
         print(np.asarray(soma.to_bytes(1,byteorder= 'big')))
+        start = time.time()
         com1.sendData(np.asarray(len(comandos).to_bytes(1,byteorder= 'big')))
         time.sleep(0.05)
 
@@ -91,32 +79,26 @@ def main():
         print(int.from_bytes(conf, byteorder='big'))
 
         if int.from_bytes(conf, byteorder='big') != len(comandos):
-            print("numero recebido diferente do enviado")
+            print("Tente novamente, o número recebido diferente do enviado")
             com1.disable()
         n =0
         while n< len(tamanhos):
+            time.sleep(2)
             com1.sendData(np.asarray(tamanhos[n].to_bytes(1, byteorder='big')))
             time.sleep(0.05)
             com1.sendData(np.asarray(comandos[n]))
             time.sleep(0.05)             
             print(comandos[n])
             n+=1
-        print("enviou")
-        
-   
-        txSize = com1.tx.getStatus()
-        print(f"Como o txSize retornou o valor {txSize}, vemos que não deu tempo de transmissão, pois ainda não há nada dentro do buffer..")     
 
-        print("Transmissão completa! O próximo passo é configurar a recepção de dados para o RX.")
-
-
-        txLen = len(comandos)
-        start = time.time()
-        rxBuffer, nRx = com1.getData(txLen)
         end = time.time()
-        print("recebeu {}" .format(rxBuffer))
-        print(f'Tempo de transmissão: {end-start} segundos.')
-
+        tempo = end - start
+        if tempo >= 10:
+            print("Timeout")
+            com1.disable()
+        else:
+            print("Enviado com sucesso")
+            print(f"Transmissão completa! Tempo de transmissão: {tempo} segundos.")
     
         # Encerra comunicação
         print("-------------------------")
--- Projeto 2 - Client Server/aplicacao_server.py
+++ Projeto 2 - Client Server/aplicacao_server.py
@@ -2,44 +2,28 @@
 # Camada Física da Computação
 #Carareto
 #11/08/2020
-#Aplicação
+#AplicaçãoServer
 ####################################################
-
-
-#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
-#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
 
 
 from enlace import *
 import time
 import numpy as np
 
-# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
-#   para saber a sua porta, execute no terminal :
-#   python -m serial.tools.list_ports
-# se estiver usando windows, o gerenciador de dispositivos informa a porta
-
-#use uma das 3 opcoes para atribuir à variável a porta usada
-#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
-#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-serialName = "COM8"                  # Windows(variacao de)
+serialName = "COM9"                  # Windows(variacao de)
 
 
 def main():
     try:
-        #declaramos um objeto do tipo enlace com o nome "com". Essa é a camada inferior à aplicação. Observe que um parametro
-        #para declarar esse objeto é o nome da porta.
-        com1 = enlace('COM8')
-        
-    
-        # Ativa comunicacao. Inicia os threads e a comunicação seiral 
+
+        com1 = enlace(serialName)
+
         com1.enable()
-        print("esperando 1 byte de sacrifício")        
+        print("Esperando 1 byte de sacrifício")        
         rxBuffer, nRx = com1.getData(1)
     
         com1.rx.clearBuffer()
         time.sleep(.1)
-        #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
         print("----------------------------------------------------------")
         print("Recepcao aberta com sucesso! Vamos ao resto do projeto!")
         print("----------------------------------------------------------")
@@ -50,14 +34,18 @@ def main():
         print(int.from_bytes(rxBuffer, byteorder='big'))
         x=0
         com = []
+        start = time.time()
         while x < (int.from_bytes(rxBuffer, byteorder='big')):
+            time.sleep(5)
             tam, nRx = com1.getData(1)
             coman, nRx = com1.getData(int.from_bytes(tam, byteorder='big'))
             print(coman)
             com.append(coman) 
             x+=1
+        end = time.time()
+        time = end - start
         print(com)
-            
+        
         # Encerra comunicação
         print("-------------------------")
         print("Comunicação encerrada")
